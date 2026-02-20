a018958307d30a87cb6260746953858416e03db2
Muhammad_Rayyan <hairay261@gmail.com>
2026-02-20 13:51:03 +0700

    1st commit

---
+++ ALGORITMA_STRUKTUR_DATA_01/NIM.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NIM {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter NIM: ");
+        long nim = scanner.nextLong();
+        int n = (int) (nim % 100);
+        if (n < 10) {
+            n += 10;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (i == 10 || i == 15) {
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.print("#");
+            } else if (i % 2 == 0) {
+                System.out.print(i);
+            } else {
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+    }
+}
