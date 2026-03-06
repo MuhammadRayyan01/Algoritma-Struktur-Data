3a8849256aa616859bd9ca25c82a2e5aebb9a9d5
Muhammad_Rayyan <hairay261@gmail.com>
2026-03-06 07:20:37 +0700

    2nd commit

--- praktikum03/MahasiswaDemo19.java
+++ praktikum03/MahasiswaDemo19.java
@@ -4,11 +4,10 @@ public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
 
-    Student19[] ArrayofMahasiswa = new Student19[3];
-
-    for(int i=0;i<3;i++){
-    ArrayofMahasiswa[i]=new Student19();
-
+    Student19[] ArrayofMahasiswa = new Student19[1];
+    
+    for(int i=0;i<1;i++){
+    ArrayofMahasiswa[i] = new Student19();
         System.out.println("Masukkan Nim : ");
         ArrayofMahasiswa[i].nim = scanner.nextLine();
         System.out.println("Masukkan Nama : ");
@@ -20,14 +19,10 @@ public static void main(String[] args) {
         scanner.nextLine();
         System.out.println("===============================");
     }
-for(int i=0;i<3;i++){
-    System.out.println("Data Mahasiswa ke-"+(i+1));
-    System.out.println("Nim : " + ArrayofMahasiswa[i].nim);
-    System.out.println("Nama : " + ArrayofMahasiswa[i].name);
-    System.out.println("Kelas : " + ArrayofMahasiswa[i].kelas);
-    System.out.println("IPK : " + ArrayofMahasiswa[i].ipk);
-    System.out.println("===============================");
+    for(int i=0;i<1;i++){
+    ArrayofMahasiswa[i].printInfo();
+    }
+}
+    }
 
-}
-}
-}
+
