e554d91cf3f219c9681be559c161f4002e57955f
Muhammad_Rayyan <hairay261@gmail.com>
2026-04-09 08:31:37 +0700

    selection sort

--- ALGORITMA_STRUKTUR_DATA_06/week06/Sorting19.java
+++ ALGORITMA_STRUKTUR_DATA_06/week06/Sorting19.java
@@ -31,6 +31,17 @@ public void selectionSort(){
         data[minIndex] = temp;
     }
 }
+public void insertionSort(){
+    for(int i=1; i<size; i++){
+        int key = data[i];
+        int j = i-1;
+        while(j>=0 && data[j]>key){
+            data[j+1] = data[j];
+            j--;
+        }
+        data[j+1] = key;
+    }
+}
 public void print(){
     for(int num : data){
         System.out.print(num +" ");
--- ALGORITMA_STRUKTUR_DATA_06/week06/SortingMain.java
+++ ALGORITMA_STRUKTUR_DATA_06/week06/SortingMain.java
@@ -1,7 +1,8 @@
 public class SortingMain {
     public static void main(String[] args) {
 		int [] a = {34, 7, 23, 32, 5, 62}; 
-        int [] b = {64, 25, 12, 22, 11};   
+        int [] b = {64, 25, 12, 22, 11};
+        int [] c = {12, 11, 13, 5, 6};
 		Sorting19 sorting = new Sorting19(a);
             System.out.println("original array");
             sorting.print();
@@ -14,6 +15,13 @@ public class SortingMain {
             sorting2.selectionSort();
             System.out.println("sorted array (selection sort)");
             sorting2.print(); 
+        Sorting19 sorting3 = new Sorting19(c);
+            System.out.println("original array");
+            sorting3.print();
+            sorting3.insertionSort();
+            System.out.println("sorted array (insertion sort)");
+            sorting3.print();
+
 	}
 }
 
