1e92a59d4691ce7c9bf9cfdd651a1b9e157e7166
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-05-08 14:37:27 +0700

    practicum1

--- algoritma12/practicum01/Main.java
+++ algoritma12/practicum01/Main.java
@@ -1,24 +1,45 @@
 package algoritma12.practicum01;
 
+import java.util.*;
 
 public class Main {
 public static void main(String[] args) {
+Scanner sc = new Scanner (System.in);
 SingleLinkedList sll = new SingleLinkedList();
+int choice;
+do{
+    System.out.println("=== input data linked list ===");
+    System.out.println("1.input data");
+    System.out.println("2.print data");
+    choice = sc.nextInt();
+    sc.nextLine();
+    switch (choice) {
+        case 1:
+            System.out.println("=======");
+            System.out.print ("input your nim: ");
+            String nim = sc.nextLine();
+            System.out.print ("input your name: ");
+            String name = sc.nextLine();
+            System.out.print("input your className:");
+            String className = sc.nextLine();
+            System.out.println("=======");
+            System.out.print("input gpa:");
+            double gpa = sc.nextDouble();
 
-Student19 std1 = new Student19("001", "Student 1", "TI-1I", 3.89);
-Student19 std2 = new Student19("002", "Student 2", "TI-1I", 3.45);
-Student19 std3 = new Student19("003", "Student 3", "TI-1I", 3.20);
-Student19 std4 = new Student19("004", "Student 4", "TI-1I", 3.00);
+            Student19 std= new Student19(nim, name, className, gpa);
+            sll.addLast(std);
+            break;
+        case 2:
+            sll.print();
+            break;
+        default:
+            System.out.println("error choice");
+            break;
+    }
+}while(choice == 1 || choice == 2 );
 
 
-sll.print();
-sll.addFirst(std4);
-sll.print();
-sll.addLast(std1);
-sll.print();
-sll.insertAfter(std3, "Student 4");
-sll.insertAt(2, std2);
-sll.print();
+
 
     }
 }
