84c8448451ed45eef94973d77d22a5bccd41fb6e
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-05-09 08:35:31 +0700

    practicum 2

--- algoritma12/practicum01/Main.java
+++ algoritma12/practicum01/Main.java
@@ -2,11 +2,14 @@ package algoritma12.practicum01;
 
 import java.util.*;
 
+
 public class Main {
 public static void main(String[] args) {
 Scanner sc = new Scanner (System.in);
 SingleLinkedList sll = new SingleLinkedList();
 int choice;
+
+
 do{
     System.out.println("=== input data linked list ===");
     System.out.println("1.input data");
@@ -36,7 +39,29 @@ do{
             System.out.println("error choice");
             break;
     }
-}while(choice == 1 || choice == 2 );
+}while(choice == 1 || choice == 2 ); 
+
+
+// PRACTICUM 2 
+System.out.println("Data at index 1 is:");
+Student19 data = sll.getData(1);
+data.print();
+
+
+int idx = sll.indexOf("Student 1");
+System.out.println("Student 1 is located at index: "+idx);
+
+System.out.println("=====remove first====");
+sll.removeFirst();
+System.out.println("======= remove last =====");
+sll.removeLast();
+System.out.println("======print again======");
+sll.print();
+System.out.println("======remove at index 0======");
+sll.removeAt(0);
+System.out.println("======print again======");
+sll.print();
+System.out.println("===========");
 
 
 
