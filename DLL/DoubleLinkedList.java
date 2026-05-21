fb0ea94cfed37f14be987b3a5d4dbbe777f8e48f
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-05-22 06:28:51 +0700

    DLL Prac1

--- DLL/DoubleLinkedList.java
+++ DLL/DoubleLinkedList.java
@@ -32,5 +32,43 @@ public class DoubleLinkedList {
             tail = newNode;
         }
     }
-    public void insertAfter()
+    public void insertAfter(String key,Student data){
+        boolean found = false;
+        Node newNode = new Node (data);
+        Node temp = head;
+        while(temp != null){
+            if(temp.data.nim.equalsIgnoreCase(key)){
+                found = true;
+                if(temp == tail){
+                    addLast(data);
+                }else{
+                    newNode.next = temp.next;
+                    newNode.prev = temp;
+                    temp.next.prev = newNode;
+                    temp.next = newNode;
+                }
+            }
+            temp = temp.next;
+        }
+        if(!found){
+        System.out.println("Node with key " + key + " not found");
+        }else{
+        System.out.println("Node with key " + key + " inserted successfully");
+        }
+    }
+    void print(){
+        if(!isEmpty()){
+            Node temp = head;
+            while(temp!=null){
+                temp.data.print();
+                temp = temp.next;
+            }
+            System.out.println("");
+        }else{
+            System.out.println("Double linked list is currently empty!!");
+        }
+    }
+
 }
+
+
